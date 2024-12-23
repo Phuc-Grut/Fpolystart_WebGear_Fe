7cb5013b1c586f0db1d2ba43a050840d692e081f
thuyngan-sketch <nt975981@gmail.com>
2024-12-24 00:28:20 +0700

    update

--- src/app/component/detail-product/detail-product.component.ts
+++ src/app/component/detail-product/detail-product.component.ts
@@ -11,14 +11,14 @@ export class DetailProductComponent  {
   @Input() product: any;  // Thông tin chi tiết sản phẩm
   @Input() showModal: boolean = false; // Kiểm tra modal có hiển thị không
   @Output() close = new EventEmitter<void>(); // Đóng modal
-
+  baseUrl: string =  'https://lacdau.com'
   closeModal() {
     this.close.emit();
   }
 
   onOverlayClick(event: MouseEvent) {
     if ((event.target as HTMLElement).classList.contains('modal-overlay')) {
-      this.closeModal(); // Đóng modal khi click vào overlay
+      this.closeModal();
     }
   }
 }
